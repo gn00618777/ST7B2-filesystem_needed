66392fe943328b24e83ba8226c4c5f2a6e9b77d0
BernieChen <s99056020@cs.nchu.edu.tw>
2015-05-14 18:09:21 +0000

    Revising detectWAN.sh to fit ethernet 1G speed

--- boot_setting/detectWAN.sh
+++ boot_setting/detectWAN.sh
@@ -6,7 +6,8 @@ while [ "$i" != "0" ]
 do
    sleep 1
    sig=`dmesg | grep "PHY: 0:07 - Link is Up - 100/Full"`
-   if [ "$sig" != "" ]; then
+   sig1=`dmesg | grep "PHY: 0:07 - Link is Up - 1000/Full"`
+   if [ "$sig" != "" ] || [ "$sig1" != "" ]; then
       
          dhclient eth1 
          break
