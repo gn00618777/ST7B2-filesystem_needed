788d3691b9fee3e7edb9ff32ea480f32b2e6596b
BernieChen <s99056020@cs.nchu.edu.tw>
2015-05-15 10:12:04 +0000

    Replace detectWAN.sh with auto_get_ip.sh

---
+++ boot_setting/auto_get_ip.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+getip() {
+      
+    dhclient eth1 &> /dev/null
+
+}
+
+declare -i x=0
+
+while [ "$x" -le 5 ]
+do
+      sleep 1
+
+      str=`ifconfig eth1 | grep "inet addr"`
+
+       if [ "$str" == "" ]; then
+
+            getip &
+
+       else
+
+           exit 0
+
+       fi
+  
+      count=`ps aux | grep -c "dhclient"`
+
+      if [ "$count" -gt "5" ]; then
+
+           killall dhclient &> /dev/null
+
+      fi  
+
+done 
